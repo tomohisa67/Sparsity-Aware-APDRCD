7bbf0b1da3bdfd216981e32d7f163a6e952dd19d
T.Tabuchi <tyoge1054a@fuji.waseda.jp>
2022-09-03 18:02:09 +0900

    edit apdrcd

--- algorithms/apdrcd.py
+++ algorithms/apdrcd.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 """
-APDRCD without duplicate sampling
+APDRCD without duplicate sampling for entropy regularized ot
 """
-
 # Author: T.Tabuchi
 # Date  : 2022/9/3
+
+import numpy as np
+
+print("apdrcd")
+
+# def apdrcd():
